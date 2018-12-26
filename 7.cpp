bf1b7c3499cec1c2a6795239cc1e98f0ba5c490b
MargaritaArofikina <44144804+MargaritaArofikina@users.noreply.github.com>
2018-12-26 23:20:15 +0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,5 +1,6 @@
 //Напишите программу, переводящую число в систему счисления с основанием k.
 
+
 #include <iostream>
 using namespace std;
 
@@ -8,20 +9,39 @@ int main() {
 int n,k;
 cin >> n >> k;
 
+
 int amount = 0;    //количество цифр в записи данной системы счисления
 for (int i = k; i < n; i*=k){
   amount ++;
 }
 amount ++;
 
+
+
 int*arr = new int [amount];
 for (int i = 0; i < amount; i++){
+    if (n%k < 10){
   *(arr + i) = n%k;
+    }
+    if (n%k >10){
+        char * c = new char [20];
+        for(char i = 'a'; i <= 't'; i++){
+            int f = n%k;
+           f -=9;
+           char g = *(c + (f));
+        
+           *(arr + i) = g;
+        }
+        
+    }
   n /= k;
+    
 }
 
 for (int i = amount - 1; i >=0; i--){
   cout << *(arr + i);
 }
+
+    
   return 0;
 }
